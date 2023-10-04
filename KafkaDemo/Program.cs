acbcd2c974eff7ec8de23f3d2526793664bc5def
Andre Damaso <adamaso@letsgetchecked.com>
2023-10-04 14:55:54 +0100

    update project

--- KafkaDemo/Program.cs
+++ KafkaDemo/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
 using Confluent.Kafka;
+using Kafka.Public;
+using Kafka.Public.Loggers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -11,8 +14,40 @@ IHostBuilder CreateHostBuilder(string[] args) =>
     Host.CreateDefaultBuilder(args)
         .ConfigureServices((context, collection) =>
         {
+            collection.AddHostedService<KafkaConsumerHostedService>();
             collection.AddHostedService<KafkaProducerHostedService>();
         });
+
+public class KafkaConsumerHostedService : IHostedService
+{
+    private readonly ILogger<KafkaConsumerHostedService> _logger;
+    private readonly ClusterClient _cluster;
+
+    public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
+    {
+        _logger = logger;
+        _cluster = new ClusterClient(new Configuration
+        {
+            Seeds = "localhost:29092"
+        }, new ConsoleLogger());
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _cluster.ConsumeFromLatest("demo");
+        _cluster.MessageReceived += record =>
+        {
+            _logger.LogInformation($"Reiceved: {Encoding.UTF8.GetString(record.Value as byte[])}");
+        };
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _cluster?.Dispose();
+        return Task.CompletedTask;
+    }
+}
 
 public class KafkaProducerHostedService : IHostedService
 {
@@ -24,7 +59,7 @@ public class KafkaProducerHostedService : IHostedService
         _logger = logger;
         var config = new ProducerConfig()
         {
-            BootstrapServers = "localhost:9092"
+            BootstrapServers = "localhost:29092"
         };
         _producer = new ProducerBuilder<Null, string>(config).Build();
     }
