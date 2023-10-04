eaa5bcb6585363d0d38e6fdb644028d0cbf0bb33
Andre Damaso <adamaso@letsgetchecked.com>
2023-10-04 11:43:28 +0100

    first commit

---
+++ KafkaDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Confluent.Kafka;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+CreateHostBuilder(args).Build().Run();
+
+IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((context, collection) =>
+        {
+            collection.AddHostedService<KafkaProducerHostedService>();
+        });
+
+public class KafkaProducerHostedService : IHostedService
+{
+    private readonly ILogger<KafkaProducerHostedService> _logger;
+    private readonly IProducer<Null, string> _producer;
+
+    public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
+    {
+        _logger = logger;
+        var config = new ProducerConfig()
+        {
+            BootstrapServers = "localhost:9092"
+        };
+        _producer = new ProducerBuilder<Null, string>(config).Build();
+    }
+    
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var value = $"Hello World {i}";
+            _logger.LogInformation(value);
+            
+            await _producer.ProduceAsync("demo", new Message<Null, string>()
+            {
+                Value = value
+            }, cancellationToken);
+        }
+
+        _producer.Flush(TimeSpan.FromSeconds(10));
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _producer?.Dispose();
+        return Task.CompletedTask;
+    }
+}
